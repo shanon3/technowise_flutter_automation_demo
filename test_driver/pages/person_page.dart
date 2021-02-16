33da552e7d8c71467bdce143813428b74b33e6e0
Shanon Athooya Saga <shanon.saga@dkatalis.com>
2021-02-17 02:10:59 +0700

    Update call library from flutter_test to test and add one more find by key

--- test_driver/pages/person_page.dart
+++ test_driver/pages/person_page.dart
@@ -10,4 +10,7 @@ class PersonPages {
   final SerializableFinder seeAllPersonButton = find.byValueKey('persona');
 
   Future<void> tapSeeAllPersonButton() async => driver.tap(seeAllPersonButton);
+
+  Future<void> tapPersonDetailByPersonName(String key) async =>
+      driver.tap(find.byValueKey(key));
 }
--- test_driver/test_suite/see_all_person_test.dart
+++ test_driver/test_suite/see_all_person_test.dart
@@ -1,20 +1,28 @@
 import 'package:flutter_driver/flutter_driver.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 
 import '../pages/person_page.dart';
 
 void main() {
   group('Persons', () {
     FlutterDriver driver;
+
     setUp(() async {
       driver = await FlutterDriver.connect();
     });
 
-    test('should be able to click the button and see the list of person',
-        () async {
-      final PersonPages personPage = PersonPages(driver: driver);
-      await personPage.tapSeeAllPersonButton();
-    }, timeout: Timeout(const Duration(minutes: 5)));
+    test(
+      'should be able to click the button and see the list of person',
+      () async {
+        final PersonPages personPage = PersonPages(driver: driver);
+        await Future.delayed(const Duration(seconds: 5));
+        await personPage.tapSeeAllPersonButton();
+        await Future.delayed(const Duration(seconds: 10));
+      },
+      timeout: Timeout(
+        const Duration(minutes: 5),
+      ),
+    );
 
     tearDown(() {
       if (driver != null) {
